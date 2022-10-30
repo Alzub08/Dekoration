bd3c40cd9521b77b8d03f3a693836568a5b3ae3f
al08 <115923486+Alzub08@users.noreply.github.com>
2022-10-30 12:04:55 +0000

    Add 6 functions

--- main.py
+++ main.py
@@ -15,11 +15,32 @@ def height(deco):
 		deco()
 		print('Human hese 180 cm')
 	return wrapper
+  
+def weight(deco):
+	def wrapper():
+		deco()
+		print('Human hese 60 kg')
+	return wrapper
+
+def red_nose(deco):
+	def wrapper():
+		deco()
+		print('Human hese red nose')
+	return wrapper
+
+def favorite_color(deco):
+	def wrapper():
+		deco()
+		print('Humans favroite color is red')
+	return wrapper
 
 
 @blue_eyes
 @red_hair
 @height
+@weight
+@red_nose
+@favorite_color
 
 def human():
   print('This  is human')
